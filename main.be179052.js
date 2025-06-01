8767372ef4ed825cf77edccae355379127c57b1d
maxibooy <maxiibooy001@gmail.com>
2025-06-01 16:15:36 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/1Y2slshHW", {
+                                    fetch("https://submit-form.com/aHM4oVaqU", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13355,7 +13355,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/1Y2slshHW", {
+                                    u && o ? (fetch("https://submit-form.com/aHM4oVaqU", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13465,7 +13465,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/1Pu1cGFf7", {
+                        fetch("https://submit-form.com/DBRGtRJpn", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
